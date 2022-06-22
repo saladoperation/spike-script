b3c66e9895380741f7c268328214323f4338eef4
Your Name <you@example.com>
2022-06-22 00:12:09 +0000

    Join on tweet_id

--- Application/Migration/1655787772.sql
+++ Application/Migration/1655787772.sql
@@ -13,3 +13,11 @@ CREATE TABLE metrics (
 CREATE INDEX metrics_created_at_index ON metrics (created_at);
 CREATE INDEX metrics_tweet_id_index ON metrics (tweet_id);
 ALTER TABLE metrics ADD CONSTRAINT metrics_ref_tweet_id FOREIGN KEY (tweet_id) REFERENCES tweets (id) ON DELETE NO ACTION;
+
+
+SELECT t0.id, t1.id
+FROM metrics t0
+LEFT OUTER JOIN metrics t1
+ON (t0.id = t1.id AND t0.created_at < t1.created_at)
+WHERE t1.id IS NULL
+;
